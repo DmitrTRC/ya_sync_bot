61ed1976b29bf770139c9672b0cc78706197c7b6
Dmitry <9984398@gmail.com>
2020-05-30 19:40:06 +0200

    Main logging and debugging configuration added.

--- sync_bot.py
+++ sync_bot.py
@@ -1,5 +1,19 @@
 import os
 import requests as rq
+import logging
+import sentry_sdk
+
 import time
 from dotenv import load_dotenv
 
+sentry_sdk.init("https://112f0bcd65ad40ad938a287a0d4ff8b9@o335977.ingest.sentry.io/5250334")
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+IS_HEROKU = os.environ.get('IS_HEROKU', False)
+if IS_HEROKU:
+    print('HEROKU IS  DETECTED ')
+else:
+    print('HEROKU IS NOT DETECTED. LOAD ENVIRONMENT FROM .ENV ')
+    load_dotenv()
